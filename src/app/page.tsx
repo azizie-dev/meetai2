b70f59fa8ffbde34ac586c1dc7fb9ea0562d1f65
azizie-dev <shahazizie360@gmail.com>
2025-11-03 19:23:00 +0800

    initial files

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center h-64 text-3xl text-green-600 font-bold">
+      <div className="mx-auto p-4">Centered vertically</div>
+    </div>
+  );
+}
