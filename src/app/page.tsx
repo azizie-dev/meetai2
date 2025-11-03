950a3b7585303ab32ef80ac972bc7f37ae7c45c3
azizie-dev <shahazizie360@gmail.com>
2025-11-03 19:31:16 +0800

    01: shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center h-64 text-3xl text-green-600 font-bold">
-      <div className="mx-auto p-4">Centered vertically</div>
-    </div>
+    <Button variant="outline">Click here</Button>
   );
 }
